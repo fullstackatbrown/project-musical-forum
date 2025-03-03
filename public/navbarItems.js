5690fc2d57423c5ae75847db8caa09833b4ff9e5
yhiffy <han_yan1@brown.edu>
2025-03-03 11:35:16 -0500

    update navbar settings

---
+++ public/navbarItems.js
@@ -0,0 +1,19 @@
+/**
+ * navItems: Navigation links used by the Navbar component.
+ * Each object contains:
+ * - name: The text displayed in the Navbar.
+ * - path: The route path to navigate to (e.g., "/aboutUs").
+ */
+export const navItems = [
+  { name: "About Us", path: "/aboutUs" },
+  { name: "Meet the Board", path: "/meetTheBoard" },
+  { name: "Alumni", path: "/alumni" },
+  { name: "Get Involved", path: "/getInvolved" },
+  { name: "Events", path: "/events" },
+  { name: "Apply to Board", path: "/applyToBoard" },
+  { name: "Proposing To Direct", path: "/proposeToDirect" },
+  { name: "Gallery", path: "/gallery" },
+  { name: "Contact Us", path: "/contactUs" },
+  { name: "Donation", path: "/donation" },
+  { name: "Shop", path: "/shop" },
+];
--- src/app/page.js
+++ src/app/page.js
@@ -1,28 +1,9 @@
 import Navbar from "@/components/Navbar";
 import TopPane from "@/components/TopPane";
 import Image from "next/image";
+import { navItems } from "../../public/navbarItems";
 
 export default function Home() {
-  /**
-   * navItems: Navigation links used by the Navbar component.
-   * Each object contains:
-   * - name: The text displayed in the Navbar.
-   * - path: The route path to navigate to (e.g., "/aboutUs").
-   */
-  const navItems = [
-    { name: "About Us", path: "/aboutUs" },
-    { name: "Meet the Board", path: "/meetTheBoard" },
-    { name: "Alumni", path: "/alumni" },
-    { name: "Get Involved", path: "/getInvolved" },
-    { name: "Events", path: "/events" },
-    { name: "Apply to Board", path: "/applyToBoard" },
-    { name: "Proposing To Direct", path: "/proposeToDirect" },
-    { name: "Gallery", path: "/gallery" },
-    { name: "Contact Us", path: "/contactUs" },
-    { name: "Donation", path: "/donation" },
-    { name: "Shop", path: "/shop" },
-  ];
-
   return (
     <div>
       <Navbar navItems={navItems} />
