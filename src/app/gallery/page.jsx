23801889846296bf8f3d8ad59c39db7dc7f98c9d
Michael Peck <ppeck64@gmail.com>
2025-03-03 18:16:43 -0500

    Spotlight WIP

--- src/app/gallery/page.jsx
+++ src/app/gallery/page.jsx
@@ -7,3 +7,5 @@ function page() {
 }
 
 export default page
+
+//2 percent of a specific 
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,26 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
-  <div className="flex justify-center items-center h-screen">
-    div className= "float-left w-1/3"
-  </div>
-  return <p> o to nextjs.org →</p>;
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div id="left-third-div" className="float-left w-1/3 h-full p-4">
+        <p>left third</p>
+        <Link href="/gallery">Gallery</Link>
+        <Link href="/board-and-alumni">Meet Board and Alumni</Link>
+        <Link href="/board-application">Apply to Board</Link>
+      </div>
+
+      <div id="middle-third-div" className="float-left w-1/3 h-full p-4">
+        <p>middle third</p>
+        {/* Logo:  <img src=""> </img> */}
+      </div>
+
+      <div id="right-third-div" className="float-left w-1/3 h-full p-4">
+        <Link href="/donation">Donation</Link>
+        <Link href="/contact-us">Contact Us</Link>
+        <Link href="/featured-event">Featured Event</Link>
+      </div>
+    </div>
+  );
 }
-//three absolute divs that lightup, and change opacity on hover,  color being lightup 
