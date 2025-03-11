0bf33b900d8c1c64960862da9d92a85bbef26128
sabrina <example.com>
2025-03-11 02:05:31 -0400

    add: footer component

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+import { Footer } from "@/components/Footer";
 import TopPane from "@/components/TopPane";
 import Image from "next/image";
 
@@ -5,6 +6,7 @@ export default function Home() {
   return (
     <div>
       <TopPane></TopPane>
+      <Footer />
     </div>
   );
 }
