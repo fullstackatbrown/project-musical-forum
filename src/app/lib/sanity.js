c4f2d4c02b525c6b5f08cbdc7e94d61b59527b65
Nitin Sudarsanam <nitinsudarsanam@yahoo.com>
2025-04-22 18:14:17 -0400

    changed database

--- src/app/lib/sanity.js
+++ src/app/lib/sanity.js
@@ -4,7 +4,7 @@ import sanityClient from '@sanity/client'
 
 // Sanity client configuration
 export const client = sanityClient({
-  projectId: 'thozhujj', // Replace with your actual project ID from Sanity
+  projectId: 'wk6uxkd9', // Replace with your actual project ID from Sanity
   dataset: 'production',        // Replace with your dataset name (usually "production")
   useCdn: true,                 // Use the CDN for faster reads; set to false for fresh data during development
   apiVersion: '2023-01-01',     // Use the latest API version; adjust as needed
